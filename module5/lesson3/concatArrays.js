50ef133db1048da31d9af3ca0cafd100273e8348
felipe <profissionalf.andre@gmail.com>
2020-07-20 14:11:53 -0300

    feat: add lesson3

---
+++ module5/lesson3/concatArrays.js
@@ -0,0 +1,8 @@
+const numbers = [1,2,3]
+const numbers2 = [4,5,6]
+let arrayTotal = numbers.concat(numbers2)
+arrayTotal = numbers.concat(numbers2,[7,8,9], false, 'Felipe')
+
+let spread = [...numbers,'Felipe',...numbers2, false,2,2.15,[],...[10,11,12]]
+console.log(arrayTotal,spread)
+
