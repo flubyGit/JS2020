16869d77eebda525fad0305086a0ed057f404e9d
Felipe <profissionalf.andre@gmail.com>
2020-08-14 01:54:46 -0300

    feat: validate props in mouterConfig

--- APIRest/src/controllers/Photo.js
+++ APIRest/src/controllers/Photo.js
@@ -1,6 +1,18 @@
-class Photo {
-  async store(res, req) {
-    console.log(req.file);
+import multer from 'multer';
+import multerConfig from '../config/multer';
+
+const upload = multer(multerConfig).single('photo');
+
+class PhotoController {
+  store(req, res) {
+    return upload(req, res, async (error) => {
+      if (error) {
+        return res.status(400).json({
+          errors: [error.code],
+        });
+      }
+      return res.json(req.file);
+    });
   }
 }
-export default new Photo();
+export default new PhotoController();
--- APIRest/src/routes/photo.js
+++ APIRest/src/routes/photo.js
@@ -1,11 +1,8 @@
 import { Router } from 'express';
-import multer from 'multer';
 import Photo from '../controllers/Photo';
-import multerConfig from '../config/mouter';
 
-const upload = multer(multerConfig);
 const router = new Router();
 
-router.post('/', upload.single('file'), Photo.store);
+router.post('/', Photo.store);
 
 export default router;
