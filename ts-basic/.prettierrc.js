4f19f0eb1a4078c43af726f0c44e5df80af4d55e
Felipe <profissionalf.andre@gmail.com>
2020-08-14 21:36:19 -0300

    feat: add module-17

---
+++ ts-basic/.prettierrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  singleQuote: true, // aspas simples
+  semi: true, // Ter ;
+  trailingComma: 'all',
+  printWidth: 80, // Largura da linha pra ele fazer o break
+  tableWidth: 2 // 2 de indentação
+}
---
+++ ts-basic/eslint.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+
+sudo yarn add @typescript-eslint/eslint-plugin @typescript-eslint/parser -D
